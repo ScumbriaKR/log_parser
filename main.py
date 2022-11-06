8f34e0726e7e6d77cd61b6ddcd5e200d8a2919fc
Maxim Krekshin <krekshin2014@gmail.com>
2022-11-07 01:24:13 +0300

    Реализация 0 ICQ

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Теперь здесь ничего нет(
+import sys
+import os
+
+if len(sys.argv) == 1:
+    print("Path to file or folder must be here :\\")
+else:
+    errors = open("errors.log", "w")
+    for filename in os.listdir(sys.argv[1]):
+        with open(os.path.join(sys.argv[1], filename), 'r') as f:
+            for line in f:
+                if line.lower().find("error") != -1:
+                    errors.write(line)
+        f.close()
+    errors.close()
